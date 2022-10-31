a417be596e57ab4c60907c0dd94cacec7d321076
Nicolai Jacob Kofod-Jensen <nicolai.kofod.jensen@gmail.com>
2022-10-31 08:24:27 +0100

    Minor commit

--- render_map.py
+++ render_map.py
@@ -9,8 +9,7 @@ gpd.read_file("./test_data/geo/opstillingskreds.shp")
 
 
 
-with open("./test_data/geo/opstilling_modified.json","r") as infile: 
-    tmp_geo = json.load(infile)
+
 
 with open("./test_data/parties.json","r") as infile:
     parties = json.load(infile)
@@ -41,7 +40,7 @@ for district in results:
     aggregated[district] = {
         "Red": tmp[0]/tmp[2],
         "Blue": tmp[1]/tmp[2],
-        #"Color": (int(255*tmp[0]/tmp[2]),0,int(255*tmp[1]/tmp[2])),
+        "Color": (tmp[0]/tmp[2],0,tmp[1]/tmp[2],1),
         "Result": tmp[1] - tmp[0], 
         "Hex_color": "#" + str(rgb_to_hex((int(255*tmp[0]/tmp[2]),0,int(255*tmp[1]/tmp[2]))))
     }
@@ -58,26 +57,31 @@ results = {
 
 for i in aggregated: 
     results["opstilling"].append(int(i))
-    results['Color'].append(aggregated[i]['Hex_color'])
+    results['Color'].append(aggregated[i]['Color'])
     results['Result'].append(aggregated[i]['Result'])
 
 
 
 with open("tmp.json","w") as outfile:
-    json.dump(aggregated,outfile,indent=4)
+    json.dump(results,outfile,indent=4)
 result_df = pd.DataFrame(results)
 result_df['opstilling'] -= 19
+print(result_df[result_df["opstilling"] == 92])
 geo = gpd.read_file("./test_data/geo/opstillingskreds.shp")
 
 geo['opstilling'] = geo['opstilling'].astype(int)
-geo = geo.join(result_df,on="opstilling",rsuffix='out')
+print(geo[geo["opstilling"] == 92])
+geo = pd.merge(geo,result_df,left_on="opstilling",right_on='opstilling')
+
+print(geo[geo["opstilling"] == 92])
+
 geo_out = pd.DataFrame()
 geo_out['opstilling'] = geo['opstilling']
 geo_out['Color'] = geo['Color']
 geo_out.to_csv("tmp.csv")
 
-ax = geo.plot(column='Result',cmap=RdBu_4.mpl_colormap)
-# ax = geo.plot(color=geo['Color'])
+#ax = geo.plot(column='Result',cmap=RdBu_4.mpl_colormap)
+ax = geo.plot(color=geo['Color'])
 
 ax.get_xaxis().set_visible(False)
 
